21f13046f97be5cec6fc8078e03355556db789b7
Sebastian Rubina <sebastian.rubina@icloud.com>
2024-08-17 22:36:16 -0400

    Add Coding Keys

--- CitySights/Models/Business.swift
+++ CitySights/Models/Business.swift
@@ -8,35 +8,49 @@
 import Foundation
 
 struct Business: Decodable, Identifiable {
-    var id: String?
-    var alias: String?
-    var categories: [Category]?
-    var coordinates: Coordinate?
-    var display_phone: String?
-    var distance: Double?
-    var image_url: String?
-    var is_closed: Bool?
-    var location: Location?
-    var name: String?
-    var phone: String?
-    var price: String?
-    var rating: Double?
-    var review_count: Int?
-    var url: String?
+    let id: String
+    let alias: String?
+    let categories: [Category]?
+    let coordinates: Coordinate?
+    let displayPhone: String?
+    let distance: Double?
+    let imageUrl: String?
+    let isClosed: Bool?
+    let location: Location?
+    let name: String?
+    let phone: String?
+    let price: String?
+    let rating: Double?
+    let reviewCount: Int?
+    let url: String?
+    
+    enum CodingKeys: String, CodingKey {
+        case id, alias, categories, coordinates, distance, location, name, phone, price, rating, url
+        case displayPhone = "display_phone"
+        case imageUrl = "image_url"
+        case isClosed = "is_closed"
+        case reviewCount = "review_count"
+    }
 }
 
 struct Category: Decodable {
-    var alias: String?
-    var title: String?
+    let alias: String?
+    let title: String?
 }
 
 struct Location: Decodable {
-    var address1: String?
-    var address2: String?
-    var address3: String?
-    var city: String?
-    var country: String?
-    var display_address: [String]?
-    var state: String?
-    var zip_code: String?
+    let address1: String?
+    let address2: String?
+    let address3: String?
+    let city: String?
+    let country: String?
+    let displayAddress: [String]?
+    let state: String?
+    let zipCode: String?
+    
+    enum CodingKeys: String, CodingKey {
+        case address1, address2, address3, city, country, state
+        case displayAddress = "display_address"
+        case zipCode = "zip_code"
+    }
 }
