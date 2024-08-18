b033d3590d5feec930847a3af4adb795579528fb
Sebastian Rubina <sebastian.rubina@icloud.com>
2024-08-17 20:13:22 -0400

    Initial Commit

---
+++ CitySights/CitySightsApp.swift
@@ -0,0 +1,17 @@
+//
+//  CitySightsApp.swift
+//  CitySights
+//
+//  Created by Sebastián Rubina on 2024-08-17.
+//
+
+import SwiftUI
+
+@main
+struct CitySightsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
