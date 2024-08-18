d1e745f24ccbd67c6cd17ec3809da1e9cebcf181
Sebastian Rubina <sebastian.rubina@icloud.com>
2024-08-17 22:11:13 -0400

    Show list of businesses

--- CitySights/ContentView.swift
+++ CitySights/ContentView.swift
@@ -11,23 +11,32 @@ struct ContentView: View {
     var service = DataService()
     
     @State var query: String = ""
+    @State var businesses = [Business]()
     
     var body: some View {
-        HStack {
-            TextField("What are you looking for?", text: $query)
-                .textFieldStyle(.roundedBorder)
-            
-            Button {
-                // TODO: Implement Query
-            } label: {
-                Text("Go")
+        VStack {
+            HStack {
+                TextField("What are you looking for?", text: $query)
+                    .textFieldStyle(.roundedBorder)
+                
+                Button {
+                    // TODO: Implement Query
+                } label: {
+                    Text("Go")
+                }
+                .buttonStyle(.borderedProminent)
             }
-            .buttonStyle(.borderedProminent)
+            
+            List(businesses) { business in
+                Text(business.name ?? "")
+            }
+            .listStyle(.plain)
         }
         .padding()
         .task {
-            let businesses = await service.businessSearch()
+            businesses = await service.businessSearch()
         }
+
     }
 }
 
